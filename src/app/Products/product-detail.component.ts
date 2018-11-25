c885da3a9f8d5549a2e6bf20b313ab97af125c96
tom@tom.cz <tom@tom.cz>
2018-11-25 23:10:42 +0100

    cmt

--- src/app/Products/product-detail.component.ts
+++ src/app/Products/product-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
