3830b36dd0ed247e5e99822be7a0147db0d00b5b
tom@tom.cz <tom@tom.cz>
2018-11-28 00:18:09 +0100

    cmt

--- src/app/Products/product-detail.component.ts
+++ src/app/Products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -6,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string = "Product Detail"
+  product: IProduct;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += 
   }
 
 }
