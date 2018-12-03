d1cf3934dfc78108536bcbc019ea065943cb5f98
tom@tom.cz <tom@tom.cz>
2018-12-03 22:55:13 +0100

    cmt

--- src/app/Products/product-detail.component.ts
+++ src/app/Products/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -11,11 +11,28 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail"
   product: IProduct;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    //snapsho
     let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += 
+    this.pageTitle += `: ${id}`;
+    this.product = 
+    {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products'])
   }
 
 }
---
+++ src/app/Products/product.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductDetailGuard } from './product-detail.guard';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module'
+
+@NgModule({
+  declarations: [
+    ProductDetailComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
+  ],
+  imports: [
+    RouterModule.forChild([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id',
+      canActivate: [ProductDetailGuard],
+      component: ProductDetailComponent
+    }
+    ]),
+    SharedModule,
+    
+  ]
+})
+export class ProductModule { }
